a1ef9ac7ce160bc1e073ad18de604b2a9d48c55e
Juan Cruz Gassó Loncan <juancruzgassoloncan@gmail.com>
2018-09-28 14:23:16 -0300

    calibration return calibdata object and brain class
    calibData has all the parameters gotten from the trackbars that could be used later in the detection fase.
    
    Also _transl() was changed as public
    
    processes
    the were created to functions to save and load calibData object to then be used by brain object

--- detec_board.py
+++ detec_board.py
@@ -17,12 +17,16 @@
 import cv2
 import numpy as np
 import helper as H
+import dill as pickle
+
 # import socket
 # import select
 # import time
 
 
-class Calib:
+
+
+class calibration:
 
     def __init__(self, camera):
         self.win_names = ('Raw', 'Canny', 'Dilation', 'Calibration')
@@ -30,16 +34,25 @@ class Calib:
         self.camera = camera
         self.cam_width = int(self.camera.get(cv2.CAP_PROP_FRAME_WIDTH))
         self.cam_height = int(self.camera.get(cv2.CAP_PROP_FRAME_HEIGHT))
-        # self.T2 = np.float32([[1, 0, 5], [0, 1, 4]])
         self.maxT = 100
         self.minT = 30
         self.k_size = 3
         self.d_iter = 3
         self.e_iter = 2
         self.roi = None
+        self.cells = None
+        class calibData:
+            roi = None
+            cells = None
+            minCannyTh = None
+            maxCannyTh = None
+            kernelSize = None
+            dilationIter = None
+            erodeIter = None
+        self.calibData = calibData()
 
-    def _transl(self, x, y):
-        self._T1 = np.float32([[1, 0, x],
+    def transl(self, x, y):
+        self.T1 = np.float32([[1, 0, x],
                                [0, 1, y]])
 
     def create_windows(self):
@@ -66,14 +79,14 @@ class Calib:
         cv2.destroyAllWindows()
 
     def get_roi(self):
-        cv2.createTrackbar('max_canny',  self.win_names[1], self.maxT, 255, lambda *args: None)
-        cv2.createTrackbar('min_canny',  self.win_names[1], self.minT, 255, lambda *args: None)
-        cv2.createTrackbar('kernel_size',  self.win_names[2], 2, 5, lambda *args: None)
-        cv2.createTrackbar('dilate_iter',  self.win_names[2], 3, 5, lambda *args: None)
-        cv2.createTrackbar('erode_iter',  self.win_names[2], 2, 5, lambda *args: None)
+        cv2.createTrackbar('max_canny', self.win_names[1], self.maxT, 255, lambda *args: None)
+        cv2.createTrackbar('min_canny', self.win_names[1], self.minT, 255, lambda *args: None)
+        cv2.createTrackbar('kernel_size', self.win_names[2], 2, 5, lambda *args: None)
+        cv2.createTrackbar('dilate_iter', self.win_names[2], 3, 5, lambda *args: None)
+        cv2.createTrackbar('erode_iter', self.win_names[2], 2, 5, lambda *args: None)
 
-        cv2.createTrackbar('transl_x',  self.win_names[3], 0, 5, lambda *args: None)
-        cv2.createTrackbar('transl_y',  self.win_names[3], 0, 5, lambda *args: None)
+        cv2.createTrackbar('transl_x', self.win_names[3], 0, 5, lambda *args: None)
+        cv2.createTrackbar('transl_y', self.win_names[3], 0, 5, lambda *args: None)
         cv2.setTrackbarMin('transl_x', self.win_names[3], -5)
         cv2.setTrackbarMin('transl_y', self.win_names[3], -5)
 
@@ -85,14 +98,13 @@ class Calib:
             self.e_iter = cv2.getTrackbarPos('erode_iter', self.win_names[2])
             x = cv2.getTrackbarPos('transl_x', self.win_names[3])
             y = cv2.getTrackbarPos('transl_y', self.win_names[3])
-            self._transl(x, y)
+            self.transl(x, y)
 
             ret, frame = self.camera.read()
 
             if not ret:
                 self.camera.set(cv2.CAP_PROP_POS_FRAMES, 0)
                 continue
-
 
             raw_frame = frame.copy()
             src_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
@@ -106,7 +118,7 @@ class Calib:
                 dilation = cv2.dilate(canny_output, kernel, iterations=self.d_iter)
                 closed = cv2.morphologyEx(dilation, cv2.MORPH_CLOSE, kernel)
                 erode = cv2.erode(closed, kernel, iterations=self.e_iter)
-                wrapped = cv2.warpAffine(erode, self._T1, (self.cam_width, self.cam_height))
+                wrapped = cv2.warpAffine(erode, self.T1, (self.cam_width, self.cam_height))
             board = H.findBiggestContour(wrapped)
 
             if board is False:
@@ -179,291 +191,170 @@ class Calib:
 
             cells = H.findCells(wrapped)
             if cells is False:
-                cv2.imshow(source_window, frame)
+                cv2.imshow(source_window, roi_frame)
                 if cv2.waitKey(5) & 0xFF == ord('q'):
                     break
                 continue
-            cells, bbox = H.sort_contours(cells, "top-to-bottom")
-            cells, bbox = H.sort_contours(cells)
 
+            cells, bbox = H.sort_cells(cells)
             # Display the resulting frame
             cnt = []
             for i in range(len(cells)):
-                cv2.drawContours(frame, cells, i, (0, 255, 0), 1)
-                M = cv2.moments(cells[i])
-                cX = int(M["m10"] / M["m00"])
-                cY = int(M["m01"] / M["m00"])
-                # cv2.circle(copy, (cX, cY), 3, (0, 255, 0), -1)
-                cnt.append((cX, cY))
-                cv2.putText(frame, str(i), (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (100, 200, 50), 2, cv2.LINE_AA)
+                cv2.drawContours(roi_frame, cells, i, (100, 255, 100), 1)
+                cv2.putText(roi_frame, str(i), bbox[i], cv2.FONT_HERSHEY_SIMPLEX, 0.7, (100, 200, 50), 2, cv2.LINE_AA)
             #            cv2.waitKey()
 
-            cv2.imshow(source_window, frame)
+            cv2.imshow(source_window, roi_frame)
 
             if cv2.waitKey(5) & 0xFF == ord('q'):
                 break
-        self.camera.release()
+        # self.camera.release()
         cv2.destroyAllWindows()
-        cells = np.array(cells)
-        cells = np.transpose(cells.reshape((3, 3))).flatten()
-        return cells
+        self.cells = np.array(cells)
+        # cells = np.transpose(cells.reshape((3, 3))).flatten()
+        return self.cells
 
     def run(self):
         self.create_windows()
         self.checkCamera()
         self.get_roi()
-        # self.show()
+        self.get_cells()
+        self.calibData.roi = self.roi
+        self.calibData.cells = self.cells
+        self.calibData.minCannyTh = self.minT
+        self.calibData.maxCannyTh = self.maxT
+        self.calibData.kernelSize = self.k_size
+        self.calibData.dilationIter = self.d_iter
+        self.calibData.erodeIter = self.e_iter
+
+        return self.calibData
 
 
-def extracCells(roi,cam=0):
-    cap = cv2.VideoCapture(cam)
+class brain(calibration):
 
-    thresh = 25 # initial threshold
-    source_window = 'Extraction'
-    cv2.namedWindow(source_window, cv2.WINDOW_GUI_EXPANDED)
+    def __init__(self, cam, calibData):
+        self.camera = cam
+        self.win_size = (800,600)
+        self.win_names = ['detector']
+        self.create_windows()
+        self.calibData = calibData
+        self.set_roi(calibData.roi)
 
+    def prosses(self, frame):
+        # src_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+        # src_gray = cv2.blur(src_gray, (3, 3))
+        canny_output = cv2.Canny(frame, self.calibData.minCannyTh, self.calibData.maxCannyTh)
+        kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (self.calibData.kernelSize,
+                                                            self.calibData.kernelSize))
+        dilation = cv2.dilate(canny_output, kernel, iterations=self.calibData.dilationIter)
+        closed = cv2.morphologyEx(dilation, cv2.MORPH_CLOSE, kernel)
+        erode = cv2.erode(closed, kernel, iterations=self.calibData.erodeIter)
+        return erode
 
-#     Check if camera opened successfully
-    if (cap.isOpened() == False):
-        print("Error opening video stream or file")
-        cap.release()
-        cv2.destroyAllWindows()
-        return None
+    def detect(self, img):
+        src_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        src_gray = cv2.blur(src_gray, (3, 3))
+        frame = self.prosses(src_gray)
+        cnt = H.findContour(frame)
+        circles = cv2.HoughCircles(src_gray, cv2.HOUGH_GRADIENT, 1.5, 50),#param1=100,param2=100,minRadius=10,maxRadius=50)
+        print (len(circles.shape))
+        if len(circles.shape) == 3:
+            a, b, c = circles.shape
+            for i in range(b):
+                cv2.circle(img, (circles[0][i][0], circles[0][i][1]), circles[0][i][2], (0, 0, 255), 3, cv2.LINE_AA)
+            return frame, 2
+        patternX = cv2.cvtColor(cv2.imread('./img/X.png'), cv2.COLOR_BGR2GRAY)
+        patternX = cv2.resize(patternX, (90, 90))
+        patternX = H.findBiggestContour2(patternX)
+        threshold = 1.5
+        for c in cnt:
+            res = cv2.matchShapes(patternX, c, 1, 0.0)
+            if res <= threshold:
+                rect = cv2.boundingRect(c)
+                x, y, w, h = H.wrap_digit(rect, 1, False)
+                cv2.rectangle(img, (x, y), (x + w, y + h), (50, 180, 200), 2)
+                return img, 1
+        return img, 0
 
-    while True:
-        ret, frame0 = cap.read()
-        if ret:
-            rows,cols,ch = frame0.shape
-            T1 = np.float32([[1,0,-4],[0,1,-3]])
-            T2 = np.float32([[1,0,5],[0,1,4]])
-            break
+    def cut_cells(self):
+        cc = []
+        for i in self.calibData.cells:
+            cell = cv2.boundingRect(i)
+            x, y, w, h = H.wrap_digit(cell, -4, False)
+            cc.append((y, y + h, x, x + w))
+        while True:
+            ret, frame = self.camera.read()
 
-    while True:
-        ret, frame = cap.read()
-        copy = frame.copy()
-#        copy = cv2.warpAffine(copy,T2,(cols,rows))
-        copy = copy[roi[0]:roi[1],roi[2]:roi[3]]
+            if not ret:
+                self.camera.set(cv2.CAP_PROP_POS_FRAMES, 0)
+                continue
 
-        frame = cv2.warpAffine(frame,T1,(cols,rows))
-        frame = frame[roi[0]:roi[1],roi[2]:roi[3]]
+            # frame = cv2.warpAffine(frame, self.T1, (self.cam_width, self.cam_height))
+            roi_frame = self.apply_roi(frame)
 
-        src_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-        src_gray = cv2.blur(src_gray, (3,3))
-        canny_output = cv2.Canny(src_gray, thresh, thresh * 2)
+            a1 = self.detect(roi_frame[cc[0][0]:cc[0][1], cc[0][2]:cc[0][3]])
+            a2 = self.detect(roi_frame[cc[1][0]:cc[1][1], cc[1][2]:cc[1][3]])
+            a3 = self.detect(roi_frame[cc[2][0]:cc[2][1], cc[2][2]:cc[2][3]])
 
-        kernel = np.ones((2,2),np.uint8)
-        dilation = cv2.dilate(canny_output,kernel,iterations = 3)
-        dilation = cv2.morphologyEx(dilation, cv2.MORPH_CLOSE, kernel)
-        dilation = cv2.erode(dilation,kernel,iterations = 3)
+            b1 = self.detect(roi_frame[cc[3][0]:cc[3][1], cc[3][2]:cc[3][3]])
+            b2 = self.detect(roi_frame[cc[4][0]:cc[4][1], cc[4][2]:cc[4][3]])
+            b3 = self.detect(roi_frame[cc[5][0]:cc[5][1], cc[5][2]:cc[5][3]])
 
-        celdas = H.findCells(dilation)
-        if celdas is False:
-            cv2.imshow(source_window,copy)
+            c1 = self.detect(roi_frame[cc[6][0]:cc[6][1], cc[6][2]:cc[6][3]])
+            c2 = self.detect(roi_frame[cc[7][0]:cc[7][1], cc[7][2]:cc[7][3]])
+            c3 = self.detect(roi_frame[cc[8][0]:cc[8][1], cc[8][2]:cc[8][3]])
+
+            row1 = np.array([a1, a2, a3])
+            row2 = np.array([b1, b2, b3])
+            row3 = np.array([c1, c2, c3])
+
+            detected = np.concatenate((row1[:, 1], row2[:, 1], row3[:, 1]), axis=0)
+            detected = np.reshape(detected, (3, 3))
+
+            row1 = [cv2.resize(x[0], (90, 90)) for x in row1]
+            row1 = np.concatenate(row1, axis=1)
+
+            row2 = [cv2.resize(x[0], (90, 90)) for x in row2]
+            row2 = np.concatenate(row2, axis=1)
+
+            row3 = [cv2.resize(x[0], (90, 90)) for x in row3]
+            row3 = np.concatenate(row3, axis=1)
+
+            complete = np.concatenate((row1, row2, row3), axis=0)
+            cv2.imshow(self.win_names[0], complete)
             if cv2.waitKey(5) & 0xFF == ord('q'):
                 break
-            continue
-        celdas, bbox = H.sort_contours(celdas,"top-to-bottom")
-        celdas, bbox = H.sort_contours(celdas)
 
-        # Display the resulting frame
-        cnt = []
-        for i in range(len(celdas)):
-            cv2.drawContours(copy, celdas, i, (0,255,0), 1)
-            M = cv2.moments(celdas[i])
-            cX = int(M["m10"] / M["m00"])
-            cY = int(M["m01"] / M["m00"])
-            #cv2.circle(copy, (cX, cY), 3, (0, 255, 0), -1)
-            cnt.append((cX,cY))
-            cv2.putText(copy, str(i),(cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.7,(100,200,50),2,cv2.LINE_AA)
-#            cv2.waitKey()
-
-        cv2.imshow(source_window,copy)
-
-        if cv2.waitKey(5) & 0xFF == ord('q'):
-            break
-    cap.release()
-    cv2.destroyAllWindows()
-    celdas = np.array(celdas)
-    celdas = np.transpose(celdas.reshape((3,3))).flatten()
-    return celdas
-
-
-def cutCells(roi, celdas, comm, cam=0):
-    cap = cv2.VideoCapture(cam)
-
-    thresh = 25 # initial threshold
-    source_window = 'Complete'
-    cv2.namedWindow(source_window, cv2.WINDOW_GUI_EXPANDED)
-
-
-#     Check if camera opened successfully
-    if (cap.isOpened() == False):
-        print("Error opening video stream or file")
-        cv2.destroyAllWindows()
-        cap.release()
-        return None
-    # Read until video is completed
-    while True:
-        ret, frame0 = cap.read()
-        if ret:
-            rows,cols,ch = frame0.shape
-            break
-    T1 = np.float32([[1,0,-4],[0,1,-3]])
-    T2 = np.float32([[1,0,2],[0,1,2]])
-    cc = []
-    for i in celdas:
-        cell = cv2.boundingRect(i)
-        x,y,w,h = H.wrap_digit(cell, 0, False)
-#        cv2.rectangle(copy, (x,y), (x+w, y+h), (0, 0, 255), 1)
-        cc.append((y, y+h, x, x+w))
-
-
-#        cv2.rectangle(copy, (x,y), (x+w, y+h), (0, 255, 0), 2)
-#        roi = copy[y:y+h, x:x+w]
-#        cv2.drawContours(roi, celdas, -1, (0,0,0), 3)
-#
-#
-#
-#
-#
-#
-#    # Convert image to gray and blur it
-##    cc = np.array(cc)
-##    cc = cc.reshape((3,3,4))
-    while True:
-
-        ret, frame = cap.read()
-#        copy2 = cv2.warpAffine(copy2,T2,(cols,rows))
-
-#        copy2 = copy2[roi[0]:roi[1],roi[2]:roi[3]]
-        frame = frame[roi[0]:roi[1],roi[2]:roi[3]]
-        frame = cv2.warpAffine(frame,T2,(cols,rows))
-
-#        copy = frame.copy()
-
-        a1 = detect(frame[cc[0][0]:cc[0][1],cc[0][2]:cc[0][3]])
-        a2 = detect(frame[cc[1][0]:cc[1][1],cc[1][2]:cc[1][3]])
-        a3 = detect(frame[cc[2][0]:cc[2][1],cc[2][2]:cc[2][3]])
-
-        b1 = detect(frame[cc[3][0]:cc[3][1],cc[3][2]:cc[3][3]])
-        b2 = detect(frame[cc[4][0]:cc[4][1],cc[4][2]:cc[4][3]])
-        b3 = detect(frame[cc[5][0]:cc[5][1],cc[5][2]:cc[5][3]])
-
-        c1 = detect(frame[cc[6][0]:cc[6][1],cc[6][2]:cc[6][3]])
-        c2 = detect(frame[cc[7][0]:cc[7][1],cc[7][2]:cc[7][3]])
-        c3 = detect(frame[cc[8][0]:cc[8][1],cc[8][2]:cc[8][3]])
-#        for c in row1:
-#            c = detect(c)
-
-        row1 = np.array([c1,b2,a3])
-        row2 = np.array([b1,c2,b3])
-        row3 = np.array([a1,a2,c3])
-
-        detected = np.concatenate((row1[:,1],row2[:,1],row3[:,1]),axis=0)
-        detected = np.reshape(detected,(3,3))
-
-        row1 = [cv2.resize(x[0],(90,90)) for x in row1]
-        row1 = np.concatenate(row1, axis=1)
-
-        row2 = [cv2.resize(x[0],(90,90)) for x in row2]
-        row2 = np.concatenate(row2, axis=1)
-
-        row3 = [cv2.resize(x[0],(90,90)) for x in row3]
-        row3 = np.concatenate(row3, axis=1)
-
-        complete = np.concatenate((row1,row2,row3),axis=0)
-
-        cv2.imshow(source_window,complete)
-        if cv2.waitKey(5) & 0xFF == ord('q'):
-            break
-#        time.sleep(0.5)
-
-
-        try:
-            data_r = conn.recv(1024)
-            if data_r == b'a':
-                conn.sendall(detected.astype('int8'))
-            elif not data_r:
-                print('server closing')
-                break
-        except:
-            pass
-#           print(detected)
-
-    cap.release()
-    cv2.destroyAllWindows()
-
-
-#
-def detect(img):
-    ficha = 0
-    frame = img
-    thresh = 25 # initial thresh.old
-#    source_window = 'Complete'0
-#    cv2.namedWindow(source_window, cv2.WINDOW_GUI_EXPANDED)
-    rows,cols,ch = frame.shape
-    T1 = np.float32([[1,0,-4],[0,1,-3]])
-    T2 = np.float32([[1,0,5],[0,1,4]])
-    src_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    src_gray = cv2.blur(src_gray, (3,3))
-    canny_output = cv2.Canny(src_gray, thresh, thresh * 2)
-    kernel = np.ones((2,2),np.uint8)
-    dilation = cv2.dilate(canny_output,kernel,iterations = 3)
-    dilation = cv2.morphologyEx(dilation, cv2.MORPH_CLOSE, kernel)
-#    dilation = cv2.erode(dilation,kernel,iterations = 2)
-    dilation = cv2.warpAffine(dilation,T1,(cols,rows))
-    cnt = H.findContour(dilation)
-    cv2.drawContours(frame, cnt, -1, (0,255,0), 1)
-    circles = cv2.HoughCircles(src_gray, cv2.HOUGH_GRADIENT, 1, 10, np.array([]), 100, 30, 1, 35)
-#    print (len(circles.shape))
-    if len(circles.shape) == 3:
-        a, b, c = circles.shape
-        for i in range(b):
-            cv2.circle(frame, (circles[0][i][0], circles[0][i][1]), circles[0][i][2], (0, 0, 255), 3, cv2.LINE_AA)
-        return frame, 2
-    patternX = cv2.cvtColor(cv2.imread('X.png'), cv2.COLOR_BGR2GRAY)
-    patternX = cv2.resize(patternX,(90,90))
-    patternX = H.findBiggestContour2(patternX)
-    threshold = 1.5
-    for c in cnt:
-        res = cv2.matchShapes(patternX, c, 1, 0.0)
-        if res <= threshold:
-            rect = cv2.boundingRect(c)
-            x,y,w,h = H.wrap_digit(rect, 1, False)
-            cv2.rectangle(frame, (x,y), (x+w, y+h), (50, 180, 200), 2)
-            return frame,1
-    return frame,0
 
 
 #    res = cv2.matchTemplate(dilation,template,cv2.TM_CCOEFF_NORMED)
 
 #
+def save_object(obj, filename):
+    with open(filename, 'wb') as output:  # Overwrites any existing file.
+        pickle.dump(obj, output, pickle.HIGHEST_PROTOCOL)
 
+def load_object(filename):
+    with open(filename, 'rb') as input:  # Overwrites any existing file.
+        return pickle.load(input)
 #    while True:
 #        cv2.imshow('dilate', dilation)
 #        if cv2.waitKey(5) & 0xFF == ord('q'):
 #            break
 
 
-
-
-          # Break the loop
+# Break the loop
 # When everything done, release the video capture object
 if __name__ == '__main__':
-    camera = H.open_camera_device(0)
-    calibration = Calib(camera)
-    # calibration.run()
-    calibration.set_roi()
-    # calibration.view_by_roi()
-    calibration.get_cells()
+    camera = H.open_camera_device('./img/video_sample.mp4')
+    # camera = H.open_camera_device(1)
 
-
-#    pitch = calib(cam)
- #   np.save('calib_board.npy',pitch)
- #    pitch = np.load('calib_board.npy')
- #    Cells = extracCells(pitch,cam)
-#    Cells = np.save('calib_cells.npy',Cells)
-##    Cells = np.load('calib_cells.npy')
+    calibrator = calibration(camera)
+    calibData = calibrator.run()
+    # save_object(calibData,'calibration.bin')
+    # calibData = load_object('calibration.bin')
+    detector = brain(camera, calibData)
+    detector.cut_cells()
 #    HOST = ''                 # Symbolic name meaning all available interfaces
 #    PORT = 50007              # Arbitrary non-privileged port
 ##    socket.setdefaulttimeout(0.5)
