974c77f58b9f9b651bc5d10ce999ca5d4d78f88c
Juan Cruz Gassó Loncan <juancruzgassoloncan@gmail.com>
2018-09-28 14:29:57 -0300

    Redefinition of sort_contours() ->sort_cells()
    The functions now sort the contours always in the same manner, from left to right and from bottom to top. Now there are calculated their centroids instead of the bounding boxs.

--- helper.py
+++ helper.py
@@ -34,19 +34,21 @@ def open_camera_device(device_number):
     camera = cv2.VideoCapture(device_number)
     return camera
 
-def wrap_digit(rect, padding = 3, center = True):
+
+def wrap_digit(rect, padding=3, center=True):
     x, y, w, h = rect
-#    padding = 5q
+    #    padding = 5q
     if center:
-        hcenter = int(x + w/2)
-        vcenter = int(y + h/2)
+        hcenter = int(x + w / 2)
+        vcenter = int(y + h / 2)
         if (h > w):
             w = h
-            x = hcenter - int(w/2)
+            x = hcenter - int(w / 2)
         else:
             h = w
-            y = vcenter - int(h/2)
-    return (x - padding, y - padding, w + 2*padding, h + 2*padding)
+            y = vcenter - int(h / 2)
+    return (x - padding, y - padding, w + 2 * padding, h + 2 * padding)
+
 
 def findBiggestContour2(mask):
     temp_bigger = []
@@ -69,44 +71,45 @@ def findBiggestContour(mask):
     _, contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     board = []
     if hierarchy is None: return False
-    for cnt, hie in zip(contours, hierarchy[0,:,:]):
+    for cnt, hie in zip(contours, hierarchy[0, :, :]):
         if hie[3] == -1 and hie[2] != -1:
-           board.append(cnt)
-    if board == []:
+            board.append(cnt)
+    if not board:
         return False
     else:
         return board
+
+
 def findContour(mask):
     img1, contours, hierarchy = cv2.findContours(mask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     return contours
 
+
 def findCells(mask):
-    ext, contours, hierarchy = cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    celdas = []
-    for cnt, hie in zip(contours, hierarchy[0,:,:]):
+    ext, contours, hierarchy = cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cells = []
+    for cnt, hie in zip(contours, hierarchy[0, :, :]):
         if hie[3] == 0:
-            celdas.append(cnt)
-    return celdas
+            cells.append(cnt)
+    if not cells:
+        return False
+    else:
+        return cells
 
-def sort_contours(cnts, method="left-to-right"):
-	# initialize the reverse flag and sort index
-	reverse = False
-	i = 0
 
-	# handle if we need to sort in reverse
-	if method == "right-to-left" or method == "bottom-to-top":
-		reverse = True
+def sort_cells(cnts):
+    centroids = []
+    for c in cnts:
+        M = cv2.moments(c)
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+        centroids.append((cX, cY))
 
-	# handle if we are sorting against the y-coordinate rather than
-	# the x-coordinate of the bounding box
-	if method == "top-to-bottom" or method == "bottom-to-top":
-		i = 1
-
-	# construct the list of bounding boxes and sort them from top to
-	# bottom
-	boundingBoxes = [cv2.boundingRect(c) for c in cnts]
-	(cnts, boundingBoxes) = zip(*sorted(zip(cnts, boundingBoxes),
-		key=lambda b:b[1][i], reverse=reverse))
-
-	# return the list of sorted contours and bounding boxes
-	return (cnts, boundingBoxes)
+    [cnts, centroids] = zip(*sorted(zip(cnts, centroids), key=lambda b: b[1][1], reverse=True))
+    reverse = False
+    row1 = sorted(zip(cnts[:3], centroids[:3]), key=lambda b: b[1][0], reverse=reverse)
+    row2 = sorted(zip(cnts[3:6], centroids[3:6]), key=lambda b: b[1][0], reverse=reverse)
+    row3 = sorted(zip(cnts[6:], centroids[6:]), key=lambda b: b[1][0], reverse=reverse)
+    cc = (*[a for a, _ in row1], *[a for a, _ in row2], *[a for a, _ in row3])
+    bb = (*[a for _, a in row1], *[a for _, a in row2], *[a for _, a in row3])
+    return cc, bb
